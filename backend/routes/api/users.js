16ac2c55336b4c1a191f5bcfa813ac52982d273b
Saim Amir <saimxamir@gmail.com>
2023-09-02 23:31:41 -0400

    add route to GET logged in user profile

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -12,48 +12,48 @@ const User = require('../../models/User');
 // @desc    Register User
 // @access  Public
 
-router.post('/',
+router.post(
+  '/',
   [
     // Validation middleware for 'name' field
-    check('name', 'Name is required')
-      .not()
-      .isEmpty(),
+    check('name', 'Name is required').not().isEmpty(),
 
     // Validation middleware for 'email' field
-    check('email','Please include a valid email')
-      .isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
 
     // Validation middleware for 'password' field
-    check('password','Please enter a password with 6 or more characters')
-      .isLength({min:6})
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
   ],
-  async(req, res) => {
-
+  async (req, res) => {
     // Extract validation errors, if any
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // If there are validation errors, send a 400 response with the errors
-      return res.status(400).json({errors: errors.array()});
+      return res.status(400).json({ errors: errors.array() });
     }
 
     // If validation passes, extract params from req.body.
-    
-    const {name, email, password} = req.body;
-    
-    try {
 
+    const { name, email, password } = req.body;
+
+    try {
       // Check if the user already exists in the database
-      let user = await User.findOne({email});
-      
+      let user = await User.findOne({ email });
+
       if (user) {
-        return res.status(400).json({errors: [{msg: 'User already exists.'}]});
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists.' }] });
       }
 
       // Get user's Gravatar image
       const avatar = gravatar.url(email, {
-        s: '200',  //default size
+        s: '200', //default size
         r: 'pg', //rating
-        d: 'mm' //default image
+        d: 'mm', //default image
       });
 
       // Create a new user instance
@@ -61,39 +61,39 @@ router.post('/',
         name,
         email,
         avatar,
-        password
+        password,
       });
 
       // Encrypt the user's password
       const salt = await bcrypt.genSalt(10);
-      
+
       user.password = await bcrypt.hash(password, salt);
-      
+
       // Save the user in the database
       await user.save();
-
 
       // Create a JWT payload containing user information
       const payload = {
         user: {
-          id: user.id
-        }
+          id: user.id,
+        },
       };
 
       // Sign the payload to create a JWT, return token
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
-        {expiresIn: 360000},
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
         (err, token) => {
-          if (err)  throw err;
-          res.json({token});
-        });
-
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
-  });
+  }
+);
 
 module.exports = router;

a407f5ea14ad9324349ae7d8a26170378a03621c
kimiakalantari <kimiakalantari97@gmail.com>
2023-09-05 22:56:41 +0500

    added myprofile page and about component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import MyProfile from './pages/MyProfile'
+import JobBoard from "./pages/JobBoard";
 import Welcome from "./pages/Welcome"
 import './index.css'
 
@@ -7,6 +10,8 @@ const App=()=> {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/my-profile' element={<MyProfile></MyProfile>}/>
+        <Route path='/job-board' element={<JobBoard></JobBoard>}/>
         <Route path='/' element={<Welcome></Welcome>} />
 
       </Routes>
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import EditProfile from "../components/EditProfile";
+import OverView from "../components/OverView";
+
+const About = (props) => {
+  const [editprofile, setEditProfile] = useState(true);
+  const [overView, setOverView] = useState(false);
+
+  const profile = () => {
+    setEditProfile(!setEditProfile);
+    setOverView(true);
+  };
+
+  const overViewFunction = () => {
+    setOverView(!overView);
+    setEditProfile(true);
+  };
+
+  return (
+    <>
+      <div className="flex bg-[#E8E7EF] pb-10 items-center justify-center text-sm xl:text-base space-x-5 font-[400]">
+        <p onClick={props.postOpen}>Posts</p>
+        <p className="text-[#394BAA]">About</p>
+        <p onClick={props.photoOpen}>Photos</p>
+      </div>
+
+      {editprofile && <EditProfile prof={profile} />}
+
+      {overView && <OverView over={overViewFunction} />}
+    </>
+  );
+};
+
+export default About;

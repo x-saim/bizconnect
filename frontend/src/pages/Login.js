5ace5ec7a18817d6d0269bc20801d98d345f4716
Mitali <mitali.kawde11@gmail.com>
2023-09-05 17:48:22 +0530

    add login form & dispatch action

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {Row, Col, Form, Input, Button} from 'antd'
+import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { userLogin } from "../redux/actions/userActions";
+
+function Login() {
+
+  const dispatch = useDispatch()
+
+  function login(values) {
+    console.log(values)
+    dispatch(userLogin(values))
+  }
+
+  return (
+    <div>
+      <h1>Login page</h1>
+
+      
+      
+      <Form onFinish={login}>
+        <Form.Item label='username' name='username' rules={[{required: true}]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label='password' name='password' rules={[{required: true}]}>
+          <Input />
+        </Form.Item>
+
+        <Button htmlType='submit'>Login</Button>
+
+        <Link to='/register'>Not registered, click here to register</Link>
+      </Form>
+
+
+    </div>
+  )
+}
+
+export default Login
+
+
+

9268ca33b52912ad2616680839ec9ed0a89a0a5b
Mitali <mitali.kawde11@gmail.com>
2023-09-05 17:41:44 +0530

    add Login, Registration, loading state & protect route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Redirect, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import UsersHome from './pages/UsersHome';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const { loading } = useSelector((state) => state.alertReducer);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {loading && (<div className="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>)}
+
+      <BrowserRouter>
+        
+          <Route path='/login' exact component={Login} />
+          <Route path='/register' exact component={Register} />
+          <ProtectedRoute path='/usersHome' exact component={UsersHome} />
+        
+      </BrowserRouter>
+   
     </div>
   );
 }
 
 export default App;
+
+
+export const ProtectedRoute = (props) => {
+  if (localStorage.getItem('user')) {
+    return <Route {...props}/>
+  } else {
+    return <Navigate replace to='/login'/>
+    // return <Redirect to='/login'/>
+  }
+}

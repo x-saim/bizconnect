ee6220622bea77a047f72831e6bee253531761e8
Mitali <mitali.kawde11@gmail.com>
2023-09-09 01:35:59 +0530

    create add post schema

---
+++ backend/models/postModel.js
@@ -0,0 +1,54 @@
+// This file is created by Mitali
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+  },
+
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        refer: 'users',
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        refer: 'users',
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+ 
+},
+{ timestamps : true }
+);
+
+module.exports = mongoose.model('posts', postSchema);

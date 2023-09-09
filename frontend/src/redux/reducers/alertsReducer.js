4153d0ac97ce5e1ae4ceb6689c46b5f3cf399c63
Saim Amir <saimxamir@gmail.com>
2023-09-08 22:43:16 -0400

    update alertReducer, create types file and alertActions

--- frontend/src/redux/reducers/alertsReducer.js
+++ frontend/src/redux/reducers/alertsReducer.js
@@ -1,13 +1,17 @@
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
 const initialState = [];
 
 export const alertsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'LOADING':
-      return {
-        ...state,
-        loading: action.payload,
-      };
+  const { type, payload } = action;
 
+  //dispatch type, we return state array with payload with new alert
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      //removes all alerts except the one that matches the payload
+      return state.filter((alert) => alert.id !== payload);
     default:
       return state;
   }

b5adde83423f2a8ca2271102c6049925e48175c8
Mitali <mitali.kawde11@gmail.com>
2023-09-10 15:12:49 +0530

    minor changes

--- backend/routes/api/postsRoute.js
+++ backend/routes/api/postsRoute.js
@@ -19,6 +19,17 @@ router.post("/addpost", async(req, res) => {
     return res.status(400).json(error);
   }
 
-})
+});
+
+
+router.get('/getallposts', async(req, res) => {
+  try {
+    const posts = await Post.find().populate('user')
+    res.send(posts)
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+
+});
 
 module.exports = router

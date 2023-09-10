f093a14afe370f10294727131c1af244467f528a
Mitali <mitali.kawde11@gmail.com>
2023-09-10 15:18:06 +0530

    implement getAllPosts action

--- frontend/src/redux/actions/postActions.js
+++ frontend/src/redux/actions/postActions.js
@@ -3,7 +3,8 @@ import { message } from 'antd';
 
 export const addPost = (values) => async (dispatch) => {
 
-  values.user = JSON.parse(localStorage.getItem('user')).id
+  // values.user = JSON.parse(localStorage.getItem('token'))._id
+  // values.user = JSON.parse(localStorage.getItem('user'))._id
   values.likes = []
   values.comments = []
 
@@ -13,6 +14,24 @@ export const addPost = (values) => async (dispatch) => {
     await axios.post('/api/posts/addpost', values);
     dispatch({ type: 'LOADING', payload: false });
     message.success('Post added successfully');
+    
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: 'LOADING', payload: false });
+    message.error('something went wrong');
+  }
+};
+
+
+export const getAllPosts = () => async (dispatch) => {
+
+  dispatch({ type: 'LOADING', payload: true });
+
+  try {
+    const response = await axios.get('/api/posts/getallposts');
+    dispatch({ type: 'LOADING', payload: false });
+    dispatch({type: 'GET_ALL_POSTS', payload: response.data})
+    message.success('got all posts successfully');
     
   } catch (error) {
     console.log(error);

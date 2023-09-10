cee50428c9cd7e9a46d4868c25bfe881fe184ee9
Mitali <mitali.kawde11@gmail.com>
2023-09-10 15:15:41 +0530

    mapping over posts

--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import { useSelector } from 'react-redux';
+import PostItem from './PostItem';
 
 const Posts = () => {
-  return <div>Posts</div>;
+
+  const { posts } = useSelector(state => state.postsReducer)
+
+  return (
+   
+    <Row justify='center'> 
+      <Col lg={12}>
+        {posts.map(post => {
+          return <PostItem post={post}/>
+        })}
+      </Col>
+    </Row>
+
+  )
 };
 
 export default Posts;

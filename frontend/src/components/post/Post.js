4f44f9d1a8f3ba687f792750405817c97f8843e7
Saim Amir <saimxamir@gmail.com>
2023-09-12 02:34:23 -0400

    update Post component

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -7,21 +7,59 @@ import PostItem from '../posts/PostItem';
 
 import { getPost } from '../../redux/actions/postActions';
 
-const Post = ({ getPost, post: { post, loading } }) => {
+const Post = ({
+  auth,
+  getPost,
+  post: {
+    post: { _id, user, firstname, lastname, avatar, text, comments, date },
+    loading,
+  },
+}) => {
   const { id } = useParams();
 
   useEffect(() => {
     getPost(id);
   }, [getPost, id]);
 
-  return <div>post</div>;
+  return (
+    <div className='container'>
+      <Link to='/publicposts' className='btn btn-dark'>
+        Back To Posts
+      </Link>
+      <div className='post bg-white p-1 my-1'>
+        <div className='avatar-container'>
+          <Link to={`/profile/${user}`}>
+            <img
+              className='round-img'
+              src={avatar}
+              alt='profile-pic'
+              style={{ height: '100px', width: '100px' }}
+            />
+          </Link>
+          <h4>
+            {firstname} {lastname}
+          </h4>
+        </div>
+
+        <div>
+          <div className='text-container'>
+            {' '}
+            <div>
+              <p className='my-1 text-overflow'>{text}</p>{' '}
+            </div>
+          </div>
+
+          <p className='post-date'>Posted on {date}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Post.propTypes = {
   getPost: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired,
 };
-
 const mapStateToProps = (state) => ({
   post: state.postReducer,
 });

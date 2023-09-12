ddb94565c62727addf492b579208f4127050f6cb
Saim Amir <saimxamir@gmail.com>
2023-09-12 06:13:50 -0400

    update Dashboard comp

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -16,14 +16,27 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import { useNavigate } from 'react-router-dom';
 import EducationTable from './EducationTable';
-
+import ExperienceTable from './ExperienceTable';
 import { addEductionField } from '../../redux/actions/profileActions';
+
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBBtn,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBIcon,
+} from 'mdb-react-ui-kit';
 
 //Components
 
 const Dashboard = ({
   getCurrentProfile,
-  user,
+  user: { user },
   profile: { profile, loading },
 }) => {
   //Upon mount, execute getCurrentProfile
@@ -39,65 +52,215 @@ const Dashboard = ({
         <Spinner />
       ) : profile !== null ? (
         <>
-          <h1 className='large text-primary'>Dashboard</h1>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}
-          >
-            <h2 className='medium'>User Information</h2>
+          <section style={{ backgroundColor: '#eee' }}>
+            <MDBContainer className='py-5'>
+              <h1 className='large text-primary'>Dashboard</h1>
+              <MDBRow>
+                <MDBCol>
+                  <MDBRow>
+                    <MDBCol lg='4'>
+                      <MDBCard className='mb-4'>
+                        <MDBCardBody className='text-center'>
+                          <MDBCardImage
+                            src={profile.user.avatar}
+                            alt='avatar'
+                            className='rounded-circle'
+                            style={{ width: '150px' }}
+                            fluid
+                          />
+                          <p className='text-muted mb-1'>{profile.status}</p>
+                          <p className='text-muted mb-4'>{profile.location}</p>
+                          <div className='d-flex justify-content-center mb-2'>
+                            <button
+                              onClick={() => navigate('/edit-profile')}
+                              className='btn btn-dark'
+                            >
+                              Update Profile
+                            </button>
+                          </div>
+                        </MDBCardBody>
 
-            <button
-              onClick={() => navigate('/edit-profile')}
-              className='btn btn-dark'
-            >
-              Edit
-            </button>
-          </Box>
+                        <MDBCardBody className='text-center'>
+                          <div className='d-flex justify-content-center mb-2'>
+                            <MDBBtn>Add Experience</MDBBtn>
 
-          <p>
-            Name: {profile.user.firstname} {profile.user.lastname}
-          </p>
+                            <MDBBtn outline className='ms-1'>
+                              Add Education
+                            </MDBBtn>
+                          </div>
+                        </MDBCardBody>
+                      </MDBCard>
 
-          <p>Company: {profile.company}</p>
-          <p>Website: {profile.website}</p>
-          <p>Location: {profile.location}</p>
-          <p>Status: {profile.status}</p>
+                      <MDBCard className='mb-4 mb-lg-0'>
+                        <MDBCardBody className='p-0'>
+                          <MDBListGroup flush className='rounded-3'>
+                            <MDBListGroupItem className='d-flex justify-content-between align-items-center p-3'>
+                              <MDBIcon fas icon='globe fa-lg text-warning' />
+                              <span>{profile.website}</span>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem className='d-flex justify-content-between align-items-center p-3'>
+                              <MDBIcon
+                                fab
+                                icon='github fa-lg'
+                                style={{ color: '#333333' }}
+                              />
+                              <span>{profile.github}</span>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem className='d-flex justify-content-between align-items-center p-3'>
+                              <MDBIcon
+                                fab
+                                icon='twitter fa-lg'
+                                style={{ color: '#55acee' }}
+                              />
+                              <span>{profile.twitter}</span>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem className='d-flex justify-content-between align-items-center p-3'>
+                              <MDBIcon
+                                fab
+                                icon='instagram fa-lg'
+                                style={{ color: '#ac2bac' }}
+                              />
+                              <span>{profile.instagram}</span>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem className='d-flex justify-content-between align-items-center p-3'>
+                              <MDBIcon
+                                fab
+                                icon='facebook fa-lg'
+                                style={{ color: '#3b5998' }}
+                              />
+                              <span>{profile.facebook}</span>
+                            </MDBListGroupItem>
+                          </MDBListGroup>
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBCol>
+                    <MDBCol lg='8'>
+                      <MDBCard className='mb-4'>
+                        <MDBCardBody>
+                          <MDBRow>
+                            <MDBCol sm='3'>
+                              <span>Full Name:</span>
+                            </MDBCol>
+                            <MDBCol sm='9'>
+                              <span className='text-muted'>
+                                {profile && profile.user.firstname}{' '}
+                                {profile && profile.user.lastname}
+                              </span>
+                            </MDBCol>
+                          </MDBRow>
+                          <hr />
+                          <MDBRow>
+                            <MDBCol sm='3'>
+                              <span>Email:</span>
+                            </MDBCol>
+                            <MDBCol sm='9'>
+                              <span className='text-muted'>
+                                {profile && profile.user.email}
+                              </span>
+                            </MDBCol>
+                          </MDBRow>
+                          <hr />
+                          <MDBRow>
+                            <MDBCol sm='3'>
+                              <span>Phone:</span>
+                            </MDBCol>
+                            <MDBCol sm='9'>
+                              <span className='text-muted'>(111) 222-3333</span>
+                            </MDBCol>
+                          </MDBRow>
+                          <hr />
+                          <MDBRow>
+                            <MDBCol sm='3'>
+                              <span>Mobile</span>
+                            </MDBCol>
+                            <MDBCol sm='9'>
+                              <span className='text-muted'>(111) 222-3333</span>
+                            </MDBCol>
+                          </MDBRow>
+                          <hr />
+                          <MDBRow>
+                            <MDBCol sm='3'>
+                              <span>Address:</span>
+                            </MDBCol>
+                            <MDBCol sm='9'>
+                              <span className='text-muted'>
+                                {profile.location}
+                              </span>
+                            </MDBCol>
+                          </MDBRow>
+                          <hr />
+                          <MDBRow>
+                            <MDBCol sm='3'>
+                              <span>Company:</span>
+                            </MDBCol>
+                            <MDBCol sm='9'>
+                              <span className='text-muted'>
+                                {profile.company}
+                              </span>
+                            </MDBCol>
+                          </MDBRow>
+                          <h2 className='medium'>Bio</h2>
+                          <p>{profile.bio}</p>
+                          <h2 className='medium'>Skills</h2>
+                          <ul>
+                            {profile.skills ? (
+                              profile.skills.map((skill, index) => (
+                                <li key={index}>{skill}</li>
+                              ))
+                            ) : (
+                              <li>No skills available.</li>
+                            )}
+                          </ul>
+                        </MDBCardBody>
+                      </MDBCard>
 
-          <h2 className='medium'>Skills</h2>
-          <ul>
-            {profile.skills ? (
-              profile.skills.map((skill, index) => <li key={index}>{skill}</li>)
-            ) : (
-              <li>No skills available.</li>
-            )}
-          </ul>
+                      {/* Rest of your profile information here */}
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'space-between',
+                        }}
+                      ></Box>
+                      <h2>Education</h2>
+                      <EducationTable
+                        rows={
+                          profile ? (
+                            profile.education
+                          ) : (
+                            <p>No education available.</p>
+                          )
+                        }
+                      />
+                      <h2 className='medium'>Experience</h2>
+                      <ExperienceTable
+                        rows={
+                          profile ? (
+                            profile.experience
+                          ) : (
+                            <p>No experience available.</p>
+                          )
+                        }
+                      />
 
-          <h2 className='medium'>Bio</h2>
-          <p>{profile.bio}</p>
-
-          <h2 className='medium'>Experience</h2>
-          {profile.experience ? (
-            profile.experience.map((experience) => (
-              <div key={experience._id}>
-                <p>Title: {experience.title}</p>
-                <p>Company: {experience.company}</p>
-                <p>Location: {experience.location}</p>
-                <p>From: {experience.from}</p>
-                <p>To: {experience.to}</p>
-                <p>Description: {experience.description}</p>
-              </div>
-            ))
-          ) : (
-            <p>No experience available.</p>
-          )}
-
-          <h2>Education</h2>
-          <EducationTable
-            rows={profile ? profile.education : <p>No education available.</p>}
-          />
+                      {profile.experience ? (
+                        profile.experience.map((experience) => (
+                          <div key={experience._id}>
+                            <p>Title: {experience.title}</p>
+                            <p>Company: {experience.company}</p>
+                            <p>Location: {experience.location}</p>
+                            <p>Description: {experience.description}</p>
+                          </div>
+                        ))
+                      ) : (
+                        <p>No experience available.</p>
+                      )}
+                    </MDBCol>
+                  </MDBRow>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </section>
         </>
       ) : (
         <>
@@ -113,7 +276,6 @@ const Dashboard = ({
             >
               Set up Profile Now
             </button>
-            {/* <Button onClick={() => navigate('/edit-profile')} variant='contained'></Button> */}
           </Box>
         </>
       )}
--- frontend/src/components/dashboard/ExperienceTable.js
+++ frontend/src/components/dashboard/ExperienceTable.js
@@ -7,51 +7,60 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-import { deleteExperienceField } from "../../redux/actions/profileActions";
-import { connect } from "react-redux";
+import { deleteExperienceField } from '../../redux/actions/profileActions';
+import { connect } from 'react-redux';
 import { setAlert } from '../../redux/actions/alertActions';
 
-const ExperienceTable = ({rows, deleteExperienceField, alert}) => {
-    return (
-      <TableContainer component={Paper} sx={{}}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Company</TableCell>
-              <TableCell align="right">Title</TableCell>
-              <TableCell align="right">Location</TableCell>
-              <TableCell align="right">From</TableCell>
-              <TableCell align="right">To</TableCell>
-              <TableCell align="right">Delete</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
+const ExperienceTable = ({ rows, deleteExperienceField, alert }) => {
+  return (
+    <TableContainer component={Paper} sx={{}}>
+      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>Company</TableCell>
+            <TableCell align='right'>Title</TableCell>
+            <TableCell align='right'>Location</TableCell>
+            <TableCell align='right'>From</TableCell>
+            <TableCell align='right'>To</TableCell>
+            <TableCell align='right'>Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
               //   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component='th' scope='row'>
+                {row.company}
+              </TableCell>
+              <TableCell align='right'>{row.title}</TableCell>
+              <TableCell align='right'>{row.location}</TableCell>
+              <TableCell align='right'>{row.from}</TableCell>
+              <TableCell align='right'>
+                {!row.to ? 'Current' : row.to}
+              </TableCell>
+              <TableCell
+                onClick={() => deleteExperienceField({ exp_id: row._id })}
+                align='right'
               >
-                <TableCell component="th" scope="row">
-                  {row.company}
-                </TableCell>
-                <TableCell align="right">{row.title}</TableCell>
-                <TableCell align="right">{row.location}</TableCell>
-                <TableCell align="right">{row.from}</TableCell>
-                <TableCell align="right">{row.to}</TableCell>
-                <TableCell onClick={() => deleteExperienceField({exp_id: row._id})} align="right">
-                    <button className='btn btn-danger'><i class="fa-solid fa-trash"></i></button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  }
+                <button className='btn btn-danger'>
+                  <i class='fa-solid fa-trash'></i>
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-  //Get the auth state
+//Get the auth state
 const mapStateToProps = (state) => ({
-    // profile: state.profileReducer.profile,
+  // profile: state.profileReducer.profile,
 });
 
-export default connect(mapStateToProps, { setAlert, deleteExperienceField })(ExperienceTable);
+export default connect(mapStateToProps, { setAlert, deleteExperienceField })(
+  ExperienceTable
+);

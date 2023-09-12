0b98ca3f85e99075229c339480fc6229af844542
Saim Amir <saimxamir@gmail.com>
2023-09-12 03:10:27 -0400

    set up jobpostReducer and actions

---
+++ frontend/src/redux/actions/jobpostActions.js
@@ -0,0 +1,123 @@
+import axios from 'axios';
+import { setAlert } from './alertActions';
+import {
+  GET_JOBPOSTS,
+  GET_JOBPOST,
+  JOBPOST_ERROR,
+  APPLY_JOB,
+  GET_APPS,
+  DELETE_JOBPOST,
+  ADD_JOBPOST,
+} from './types';
+
+// Get job posts
+export const getJobPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/jobposts');
+
+    dispatch({
+      type: GET_JOBPOSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: JOBPOST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get job post
+export const getJobPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/jobposts/${id}`);
+
+    dispatch({
+      type: GET_JOBPOST,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: JOBPOST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// // Apply for a job
+// export const applyForJob = (jobPostId) => async (dispatch) => {
+//   try {
+//     const res = await axios.post(`/api/jobposts/apply/${jobPostId}`);
+
+//     dispatch({
+//       type: APPLY_JOB,
+//       payload: res.data,
+//     });
+
+//     dispatch(setAlert('Application submitted successfully.', 'success'));
+//   } catch (err) {
+//     dispatch({
+//       type: JOBPOST_ERROR,
+//       payload: { msg: err.response.statusText, status: err.response.status },
+//     });
+//   }
+// };
+
+// // Get job applications
+// export const getJobApplications = () => async (dispatch) => {
+//   try {
+//     const res = await axios.get('/api/jobposts/applications');
+
+//     dispatch({
+//       type: GET_APPS,
+//       payload: res.data,
+//     });
+//   } catch (err) {
+//     dispatch({
+//       type: JOBPOST_ERROR,
+//       payload: { msg: err.response.statusText, status: err.response.status },
+//     });
+//   }
+// };
+
+// Delete job post
+export const deleteJobPost = (jobPostId) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/jobposts/${jobPostId}`);
+
+    dispatch({
+      type: DELETE_JOBPOST,
+      payload: jobPostId,
+    });
+
+    dispatch(setAlert('Job post deleted.', 'success'));
+  } catch (err) {
+    dispatch({
+      type: JOBPOST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add job post
+export const addJobPost = (formData) => async (dispatch) => {
+  const config = {
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  try {
+    const res = await axios.post('/api/jobposts', formData, config);
+
+    dispatch({
+      type: ADD_JOBPOST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Job post created.', 'success'));
+  } catch (err) {
+    dispatch({
+      type: JOBPOST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
--- frontend/src/redux/reducers/jobpostReducer.js
+++ frontend/src/redux/reducers/jobpostReducer.js
@@ -1,3 +1,4 @@
+// jobPostReducer.js
 import {
   GET_JOBPOSTS,
   GET_JOBPOST,
@@ -9,23 +10,65 @@ import {
 } from '../actions/types';
 
 const initialState = {
-  jobposts: [],
-  jobpost: null,
+  jobPosts: [],
+  jobPost: null,
   loading: true,
   error: {},
+  applications: {}, // Change applications from an array to an object
 };
 
-export const jobpostReducer = (state = initialState, action) => {
+export const jobPostReducer = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case GET_JOBPOSTS:
       return {
         ...state,
-        jobposts: payload,
+        jobPosts: payload,
         loading: false,
       };
-
+    case GET_JOBPOST:
+      return {
+        ...state,
+        jobPost: payload,
+        loading: false,
+      };
+    case ADD_JOBPOST:
+      return {
+        ...state,
+        jobPosts: [payload, ...state.jobPosts],
+        loading: false,
+      };
+    case DELETE_JOBPOST:
+      return {
+        ...state,
+        jobPosts: state.jobPosts.filter((post) => post._id !== payload),
+        loading: false,
+      };
+    case APPLY_JOB:
+      return {
+        ...state,
+        jobPost: {
+          ...state.jobPost,
+          applications: {
+            ...state.jobPost.applications,
+            [payload.applicantId]: payload.applicationData,
+          },
+        },
+        loading: false,
+      };
+    case GET_APPS:
+      return {
+        ...state,
+        applications: payload,
+        loading: false,
+      };
+    case JOBPOST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
     default:
       return state;
   }

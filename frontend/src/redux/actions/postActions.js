22b4388cb6a45bfcf8620dc0a19f13f35d1d525f
Saim Amir <saimxamir@gmail.com>
2023-09-12 02:37:21 -0400

    credate postActions for add and delete comment

--- frontend/src/redux/actions/postActions.js
+++ frontend/src/redux/actions/postActions.js
@@ -155,3 +155,41 @@ export const deletePost = (postID) => async (dispatch) => {
     });
   }
 };
+
+// Add comment
+export const addComment = (postId, formData) => async (dispatch) => {
+  try {
+    const res = await axios.post(`/publicposts/comment/${postId}`, formData);
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Comment Added.', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    await axios.delete(`/publicposts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId,
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};

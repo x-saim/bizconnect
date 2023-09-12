cc2f2267f28c736f9030bb8f81c24c79a01b1e8d
Saim Amir <saimxamir@gmail.com>
2023-09-11 23:59:13 -0400

    update postModel to pass name properly to Profile comp

--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -27,14 +27,16 @@ const PostItem = ({
   unlikePost,
   deletePost,
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date },
+  post: { _id, text, firstname, lastname, avatar, user, likes, comments, date },
 }) => {
   return (
     <div className='post bg-white p-1 my-1'>
       <div>
         <Link to={`/profile/${user}`}>
           <img className='round-img' src={avatar} alt='' />
-          <h4>{name}</h4>
+          <h4>
+            {firstname} {lastname}
+          </h4>
         </Link>
       </div>
       <div>

ff914315212df10557d9e56de39495d7f9846c51
Saim Amir <saimxamir@gmail.com>
2023-09-14 01:29:28 -0400

    add profile edu component to profile

--- frontend/src/components/profile/ProfileEducation.js
+++ frontend/src/components/profile/ProfileEducation.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
 
-const ProfileEducation = () => {
-  return <div>ProfileEducation</div>;
+const ProfileEducation = ({
+  education: { school, degree, fieldofstudy, current, to, from, description },
+}) => {
+  const formatFromDate = moment(from).format('YYYY-MM-DD');
+  const formatToDate = moment(to).format('YYYY-MM-DD');
+  return (
+    <div>
+      <h3 className='text-dark'>{school}</h3>
+      <p>
+        {formatFromDate} - {formatToDate ? formatToDate : 'Now'}
+      </p>
+      <p>
+        <strong>Degree: </strong> {degree}
+      </p>
+      <p>
+        <strong>Field Of Study: </strong> {fieldofstudy}
+      </p>
+      <p>
+        <strong>Description: </strong> {description}
+      </p>
+    </div>
+  );
+};
+
+ProfileEducation.propTypes = {
+  education: PropTypes.object.isRequired,
 };
 
 export default ProfileEducation;
